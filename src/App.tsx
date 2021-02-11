b977fb2edeefd6c39161e53d0bad32c3db65f644
axdra <axel.draws@outlook.com>
2021-02-11 23:42:06 +0100

    Doing some React testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { AppBar, Box, Breadcrumbs, Button, Drawer, IconButton, Link, Snackbar, Switch, Toolbar, Typography } from '@material-ui/core';
+import { AppBar,Paper, Box, Breadcrumbs, Button, Drawer, IconButton, Link, Snackbar, Switch, Toolbar, Typography, Grid, makeStyles, Card, CardContent, CardActions } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: "275px",
+    cursor: "pointer"
+  },
+  clients: {
+    margin: "0px 20px 0px 20px"
+  }, bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+}));
+
 function App() {
+  var l = [];
+  const classes = useStyles();
+  for (let i = 0; i < 10; i += 1) {
+    l.push(
+    <Grid item xs={4} style = {{maxWidth: "250px"}}>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Word of the Day
+        </Typography>
+            <Typography variant="h5" component="h2">
+              be{i}nev{i}o{i}lent
+        </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              adjective
+        </Typography>
+            <Typography variant="body2" component="p">
+              well meaning and kindly.
+          <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+    </Grid>)
+  }
   return (
     <div className="App">
       <AppBar position="static">
@@ -21,24 +76,20 @@ function App() {
       <Drawer anchor="left" open={false} >
       <Button>xd</Button> 
     </Drawer>
-</Box>
-<Breadcrumbs aria-label="breadcrumb">
-  <Link color="inherit" href="/" >
-    Material-UI
-  </Link>
-  <Link color="inherit" href="/getting-started/installation/" >
-    Core
-  </Link>
-  <Typography color="textPrimary">Breadcrumb</Typography>
-</Breadcrumbs>
-<Snackbar open={true} autoHideDuration={10}>
-  <a>test</a>
-</Snackbar>
-
-    </div>
+      </Box>
+      <div className={classes.clients}>
+        <Grid container spacing={3} >
+          
+        {l}
+       
+      </Grid>
+      </div></div>
   );
 }
 
+function clickHandler() {
+  console.log("xd")
+}
 export default App;
 
 
