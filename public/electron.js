8dac559a512359d942c45d00229ec4b0d47b47e6
Axel Draws <axel.draws@outlook.com>
2021-02-17 04:04:28 +0100

    Some work on layout and state management

--- public/electron.js
+++ public/electron.js
@@ -12,6 +12,9 @@ function createWindow() {
     mainWindow = new BrowserWindow({ width: 900, height: 680 });
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
     mainWindow.on('closed', () => mainWindow = null);
+    if(!isDev){
+    mainWindow.setMenu(null);
+    }
 }
 
 app.on('ready', createWindow);
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import { AppBar,Paper, Box, Breadcrumbs, Button, Drawer, IconButton, Link, Snackbar, Switch, Toolbar, Typography, Grid, makeStyles, Card, CardContent, CardActions, Fab, Icon, Badge } from '@material-ui/core';
+import { AppBar,  Box, Button, Drawer, IconButton, Toolbar, Typography, Grid, makeStyles, Card, CardContent, CardActions, Fab,  Tooltip } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Add, Favorite } from '@material-ui/icons';
 import { green, red } from '@material-ui/core/colors';
-import { randomFillSync, randomInt } from 'crypto';
+import { Console } from 'console';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   fab: {
-    position: 'absolute',
+    position: 'fixed',
     bottom: theme.spacing(2),
     right: theme.spacing(2),
   },
@@ -20,9 +20,9 @@ const useStyles = makeStyles((theme) => ({
   {
     position: 'relative',
     right: theme.spacing(2)
-    },
+  },
   paper: {
-    
+
     textAlign: 'center',
     color: theme.palette.text.secondary,
     height: "275px",
@@ -40,69 +40,94 @@ const useStyles = makeStyles((theme) => ({
   },
   pos: {
     marginBottom: 12,
+  },content: {
+   
+  },clientCard: {
+   cursor: "pointer !important" 
   },
 }));
-
 function App() {
+  const [View,setView] = useState("Clients");
+  const [sideBar,setSideBar] = useState(false);
+  
   var l = [];
   const classes = useStyles();
-  
+  let content;
   for (let i = 0; i < 10; i += 1) {
+    let randomValue = Math.floor(Math.random() * Math.floor(2));
     l.push(
-    <Grid item xs={4} style = {{maxWidth: "250px"}}>
-        <Card className={classes.root}>
+      <Grid item xs={6} style={{ maxWidth: "250px" }}>
+        <Card className={classes.root + classes.clientCard} onClick={()=>setView("ClientView")}>
           <CardContent>
             <Typography className={classes.title} color="textSecondary" gutterBottom>
-              Client { i}
+              Client {i}
             </Typography>
-            
-            <Favorite className={classes.health} style={Math.floor(Math.random() * Math.floor(2)) == 1 ? { color: green[500] } : { color: red[500] }}></Favorite>
-                        
-        
+            <Tooltip title={randomValue === 1 ? "Online" : "Offline"} aria-label="add " placement="top">
+              <Favorite className={classes.health} style={randomValue === 1 ? { color: green[500] } : { color: red[500] }}></Favorite>
+
+            </Tooltip>
+
+
           </CardContent>
           <CardActions>
             <Button size="small">Learn More</Button>
           </CardActions>
         </Card>
-    </Grid>)
+      </Grid>);
+  }
+  if(View === "Clients"){
+    content = (
+      <div className={classes.content}>
+      <Fab className={classes.fab} onClick={()=>setView("NewClient")}>
+      <Add />
+
+    </Fab>
+    <div className={classes.clients}>
+      <Grid container spacing={3} >
+        
+      {l}
+     
+      </Grid>
+      
+    </div>
+    </div>
+    );
   }
   return (
     <div className="App">
-      <Fab className={classes.fab}>
-        <Add />
 
-      </Fab>
       <AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu" >
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" className="title">
-      News
+        <Toolbar>
+          <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu" >
+            <MenuIcon onClick={()=>ToggleSideBar(sideBar,setSideBar)}/>
+          </IconButton>
+          <Typography variant="h6" className="title">
+            {View}
     </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
+        </Toolbar>
+      </AppBar>
+
+
       <Box component="span" m={1}>
-      <Drawer anchor="left" open={false} >
-      <Button>xd</Button> 
-    </Drawer>
+        <Drawer anchor="left" open={sideBar} >
+          <Button onClick={()=>ToggleSideBar(sideBar,setSideBar)}>Close</Button>
+          <Button onClick={()=>setView("Clients")}>Back</Button>
+
+        </Drawer>
       </Box>
-      <div className={classes.clients}>
-        <Grid container spacing={3} >
-          
-        {l}
-       
-        </Grid>
-        
-      </div>
-    
+      {content}
+
+
     </div>
   );
 }
 
 function clickHandler() {
   console.log("xd")
+}
+function ToggleSideBar(sidebarstate:boolean,sidebarstatefunc:any){
+  sidebarstatefunc(!sidebarstate);
+  console.log(sidebarstate);
 }
 export default App;
 
